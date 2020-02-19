43869bf04f9f755396d0db9ad8cb7a664c6ea529
Kaxaru <khidechi@gmail.com>
2020-02-19 20:42:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    return array.reduce((a,b)=> Math.min(a,b), 0) 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    return array.reduce((a,b) => Math.max(a,b), 0) 
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    return array.reduce((a,b) => a + b, 0)/array.length || 0 
 }
